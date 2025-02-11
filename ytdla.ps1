4b934e0e824c3bc39a970e7cf230d4a0edee03eb
arfshl <98870352+arfshl@users.noreply.github.com>
2025-02-11 22:09:55 +0700

    Update ytdla.ps1

--- ytdla.ps1
+++ ytdla.ps1
@@ -1,35 +1,61 @@
 [CmdletBinding()]
+  
 param (
+  
     [Parameter(Position=0)]
+  
     [string]$Mode = "-v",
+  
     
+  
     [Parameter(Position=1, Mandatory=$false)]
+  
     [string]$Url
+  
 )
+  
 
+  
 # Format flags
-$VIDEO = "-S vcodec:h264,fps,res:360,acodec:aac,ext:mp4 -P D:/Downloads"
-$AUDIO = "-x --audio-format mp3 -P D:/Downloads"
+  
+$VIDEO = "-S vcodec:h264,fps,res:360,acodec:aac,ext:mp4 -P D:\Download"
+  
+$AUDIO = "-x --audio-format mp3 -P D:\Download"
+  
 
+  
 # Mode selection
+  
 if ($Mode -notin @("-v", "-a")) {
+  
     $Url = $Mode
+  
     $Mode = "-v"
+  
 }
+  
 $FORMAT = if ($Mode -eq "-v") { $VIDEO } else { $AUDIO }
+  
 
+  
 # Download
+  
 if (-not $Url) {
+  
     Write-Host "Usage: ytdla [-v {video} |-a {audio}] <URL>"
+  
     Write-Host "Examples:"
-    Write-Host "  ytdla -v <URL>"
-    Write-Host "  ytdla -a <URL>"
+  
+    Write-Host "  .\ytdla -v <URL>"
+  
+    Write-Host "  .\ytdla -a <URL>"
+  
     exit 1
+  
 }
+  
 
-# Clean URL by removing anything after &
-$CleanUrl = $Url -replace '&.*$', ''
-
-# Execute yt-dlp
-$processArgs = @($FORMAT.Split(" ") + $CleanUrl)
-& yt-dlp $processArgs
+  
+$args = ($FORMAT + " " + $Url).Split(" ")
+  
+Start-Process -NoNewWindow -Wait -FilePath "yt-dlp" -ArgumentList $args
