306d512a2094a0c04fb67728c648d3c29eaf4b34
arfshl <98870352+arfshl@users.noreply.github.com>
2025-02-21 08:19:49 +0700

    Update goodbyedpi_run_idn.cmd

--- goodbyedpi_run_idn.cmd
+++ goodbyedpi_run_idn.cmd
@@ -5,7 +5,7 @@ IF "%PROCESSOR_ARCHITECTURE%"=="AMD64" (set _arch=x86_64)
 IF DEFINED PROCESSOR_ARCHITEW6432 (set _arch=x86_64)
 PUSHD "%_arch%"
 
-start "" goodbyedpi.exe -5 -p -f 2 -e 2 --auto-ttl --reverse-frag --max-payload
+start "" goodbyedpi.exe -p -f 2 -e 2 --reverse-frag
 
 POPD
 POPD
